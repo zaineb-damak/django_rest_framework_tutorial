cd342c3871d43c9b2504d7df433846b785e92c28
zaineb-damak <77733791+zaineb-damak@users.noreply.github.com>
2023-12-30 14:26:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# django_rest_framework_quickstart
+
+
+# Getting Started
+
+First clone the repository from Github and switch to the new directory:
+
+    $ git clone git@github.com/zaineb-damak/django_rest_framework_quickstart.git
+    $ cd django_rest_framework_quickstart
+    
+Activate the virtualenv for your project.
+    
+Install project dependencies:
+
+    $ pip install -r requirements.txt
+    
+    
+Then simply apply the migrations:
+
+    $ python manage.py migrate
+    
+
+You can now run the development server:
+
+    $ python manage.py runserver
---
+++ snippets/views.py
@@ -0,0 +1,41 @@
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from django.contrib.auth.models import User
+from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import permissions,renderers,viewsets
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This ViewSet automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
